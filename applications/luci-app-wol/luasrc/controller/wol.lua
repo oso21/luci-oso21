be59f3b1b0b6f20ec06d7f8c7812554d98d8c008
oso21 <258008858@qq.com>
2017-08-12 18:25:08 +0800

    delete redundant

--- applications/luci-app-wol/luasrc/controller/wol.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.wol", package.seeall)
-
-function index()
-	entry({"admin", "services", "wol"}, cbi("wol"), _("Wake on LAN"), 90)
-	entry({"mini", "services", "wol"}, cbi("wol"), _("Wake on LAN"), 90)
-end
